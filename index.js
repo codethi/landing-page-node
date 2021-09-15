0a7fc2892b41add3fb37bb9e820be933cd9c45f9
codethi <docencia.thiago@gmail.com>
2021-09-15 14:09:15 -0300

    ejs

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`));
+app.get("/index", (req, res) => {
+  res.render("../views/index");
+});
+
+app.listen(port, () =>
+  console.log(`Servidor rodando em http://localhost:${port}`)
+);
