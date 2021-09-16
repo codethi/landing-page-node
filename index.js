15480c25a72b7ca6a3de50eb4c1168c9f07405aa
codethi <docencia.thiago@gmail.com>
2021-09-16 16:33:09 -0300

    projeto completo

--- index.js
+++ index.js
@@ -3,18 +3,32 @@ const path = require("path");
 
 const app = express();
 const port = 3000;
+let message = "";
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded());
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-app.get("/index", (req, res) => {
   const devList = ["Backend", "Frontend", "Fullstack"];
   const analyticsList = ["Engenharia de dados", "Ciência de dados"];
-  res.render("index", { titulo: "Blue", devList: devList, analyticsList: analyticsList});
+  
+  setTimeout(() => {
+    message = "";
+  }, 1000);
+
+  res.render("index", {
+    titulo: "Blue",
+    devList: devList,
+    analyticsList: analyticsList,
+    message,
+  });
+});
+
+app.post("/subscription", (req, res) => {
+  const { nome, email } = req.body;
+  message = `Parabéns ${nome}, sua inscrição foi realizada com sucesso! Um e-mail foi enviado para: ${email}`;
+  res.redirect("/");
 });
 
 app.listen(port, () =>
